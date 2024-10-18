4d3fad6a1ab81443e2de78ba3bbc5901b61382b0
Leonardo Winck <leowinck@gmail.com>
2024-10-17 21:06:49 -0300

    User entitty and resource

---
+++ src/main/java/com/leowinck/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.leowinck.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.leowinck.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"maria","maria@gmail.com","9999999999","1234");
+		return ResponseEntity.ok().body(u);		
+	}
+}
